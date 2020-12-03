18b27e6234cc73cac7483d086892b08979bb30f3
AKgroup2000 <65353307+AKgroup2000@users.noreply.github.com>
2020-12-03 15:59:25 +0530

    Update exp3.py

--- Exp3/exp3.py
+++ Exp3/exp3.py
@@ -24,4 +24,68 @@ Minv = np.linalg.inv(M)
 print("Inversae of M : \n",Minv)
 
 
+# Without Numpy:
+
+
+def print_matrix(Title, M):
+    print(Title)
+    for row in M:
+        print([round(x,3)+0 for x in row])
+        
+def print_matrices(Action, Title1, M1, Title2, M2):
+    print(Action)
+    print(Title1, '\t'*int(len(M1)/2)+"\t"*len(M1), Title2)
+    for i in range(len(M1)):
+        row1 = ['{0:+7.3f}'.format(x) for x in M1[i]]
+        row2 = ['{0:+7.3f}'.format(x) for x in M2[i]]
+        print(row1,'\t', row2)
+        
+def zeros_matrix(rows, cols):
+    A = []
+    for i in range(rows):
+        A.append([])
+        for j in range(cols):
+            A[-1].append(0.0)
+
+    return A
+
+def copy_matrix(M):
+    rows = len(M)
+    cols = len(M[0])
+
+    MC = zeros_matrix(rows, cols)
+
+    for i in range(rows):
+        for j in range(rows):
+            MC[i][j] = M[i][j]
+
+    return MC
+
+def matrix_multiply(A,B):
+    rowsA = len(A)
+    colsA = len(A[0])
+
+    rowsB = len(B)
+    colsB = len(B[0])
+
+    if colsA != rowsB:
+        print('Number of A columns must equal number of B rows.')
+        sys.exit()
+
+    C = zeros_matrix(rowsA, colsB)
+
+    for i in range(rowsA):
+        for j in range(colsB):
+            total = 0
+            for ii in range(colsA):
+                total += A[i][ii] * B[ii][j]
+            C[i][j] = total
+
+    return C
+
+A = [[5,4,3,2,1],[4,3,2,1,5],[3,2,9,5,4],[2,1,5,4,3],[1,2,3,4,5]]
+I = [[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]]
+print_matrix('A Matrix', A)
+print()
+print_matrix('I Matrix', I)
 
