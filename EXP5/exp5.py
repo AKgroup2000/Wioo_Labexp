4334d3308291c9b65dc04eae9c5c350b989fa1e2
AKgroup2000 <65353307+AKgroup2000@users.noreply.github.com>
2021-01-02 01:42:35 +0530

    Update exp5.py

--- EXP5/exp5.py
+++ EXP5/exp5.py
@@ -1,3 +1,54 @@
+** * * *  * EXP 5.1 
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# function that returns dy/dt
+def model(y,t):
+    k = 2
+    dydt = -k * y
+    return dydt
+
+# initial condition
+y0 = 5
+
+# time points
+t = np.linspace(0,20)
+
+# solve ODE
+y = odeint(model,y0,t)
+
+# plot results
+plt.plot(t,y)
+plt.xlabel('time')
+plt.ylabel('y(t)')
+plt.show()
+
+* * * * * * EXP 5.2 
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# function that returns dy/dt
+def model(y,t):
+    dydt = 5*(np.exp(-t/3))
+    return dydt
+
+# initial condition
+y0 = 5
+
+# time points
+t = np.linspace(0,20)
+
+# solve ODE
+y = odeint(model,y0,t)
+
+# plot results
+plt.plot(t,y)
+plt.title('5e( −t/RC) Where RC = 3')
+plt.xlabel('time')
+plt.ylabel('y(t)')
+plt.show()
 
 
 
@@ -20,3 +71,26 @@ plt.plot(xs, ys, 'r*')
 plt.xlabel('t values')
 plt.ylabel('x values')
 plt.title('(D**2+2*D+2)x = e**(-t)')
+
+* * * ** * * * EXP 5.4 *** * * 
+from scipy import integrate
+from pylab import * # for plotting commands
+
+def rlc(A,t):
+    Vc,x=A
+    V = 5.0 #voltageSource
+    R = 1.0 # 1 Ohm
+    L=1.0e-6 #1mH
+    C = 1.0e-6 #1microF
+    res=array([x,(V-Vc-(x*R*C))/(L*C)])
+    return res
+
+time = linspace(0.0,0.6e-6,1001)
+vc,x = integrate.odeint(rlc,[0.0,0.0],time).T
+i = 5*(np.exp(-x))
+figure()
+plot(time,vc)
+plt.title('RLC circuit with R = 1Ω,L = 1 mH and C = 1 μF')
+xlabel('t')
+ylabel('Vc')
+show()
